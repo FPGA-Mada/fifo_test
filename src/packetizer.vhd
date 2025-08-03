201fd0db4eb030502439ebc4279fca06e39157f8
Nambinina Rakotojaona <53557693+FPGA-Mada@users.noreply.github.com>
2025-08-03 20:04:39 +0200

    add prefetch data

--- src/packetizer.vhd
+++ src/packetizer.vhd
@@ -1,3 +1,52 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity packetization is
+  generic (
+    DATA_WIDTH : positive := 32;
+    FIFO_DEPTH : positive := 64
+  );
+  Port (
+    clk     : in  std_logic;
+    rst     : in  std_logic;
+    -- AXI Stream Slave
+    s_valid : in  std_logic;
+    s_ready : out std_logic;
+    s_data  : in  std_logic_vector (DATA_WIDTH - 1 downto 0);
+    -- AXI Stream Master
+    m_valid : out std_logic;
+    m_ready : in  std_logic;
+    m_data  : out std_logic_vector (DATA_WIDTH - 1 downto 0)
+  );
+end packetization;
+
+architecture Behavioral of packetization is
+
+  -- FIFO output signals
+  signal in_data_fifo  : std_logic_vector(DATA_WIDTH - 1 downto 0);
+  signal in_valid_fifo : std_logic;
+  signal in_ready_fifo : std_logic;
+
+  -- Internal output signals
+  signal m_valid_sig : std_logic := '0';
+  signal m_data_sig  : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
+
+  -- State machine definition
+  type state_t is (send_head, send_body, send_tail);
+  signal state : state_t := send_head;
+
+  signal counter_data_send : integer range 0 to 4 := 0;
+
+begin
+
+  -- Output assignments
+  m_valid <= m_valid_sig;
+  m_data  <= m_data_sig;
+
+  --------------------------------------------------------------------
+  -- Packetizer FSM Process
+  --------------------------------------------------------------------
 packetizer_proc : process (clk)
 begin
   if rising_edge(clk) then
@@ -78,3 +127,37 @@ begin
     end if;
   end if;
 end process;
+
+  --------------------------------------------------------------------
+  -- FIFO Instantiation
+  --------------------------------------------------------------------
+  fifo_inst : entity work.olo_base_fifo_sync
+    generic map (
+      Width_g         => DATA_WIDTH,
+      Depth_g         => FIFO_DEPTH,
+      AlmFullOn_g     => false,
+      AlmFullLevel_g  => 0,
+      AlmEmptyOn_g    => false,
+      AlmEmptyLevel_g => 0,
+      RamStyle_g      => "auto",
+      RamBehavior_g   => "RBW",
+      ReadyRstState_g => '1'
+    )
+    port map (
+      Clk         => clk,
+      Rst         => rst,
+      In_Data     => s_data,
+      In_Valid    => s_valid,
+      In_Ready    => s_ready,
+      In_Level    => open,
+      Out_Data    => in_data_fifo,
+      Out_Valid   => in_valid_fifo,
+      Out_Ready   => in_ready_fifo,
+      Out_Level   => open,
+      Full        => open,
+      AlmFull     => open,
+      Empty       => open,
+      AlmEmpty    => open
+    );
+
+end Behavioral;
